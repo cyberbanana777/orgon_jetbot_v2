2e2011c21d62bd59f200e3853f09229034e133d1
cyberbanana777 <sashagrachev2005@gmail.com>
2026-03-21 20:30:09 +0300

    Fix this

--- build/aruco_analyzer/colcon_command_prefix_setup_py.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/pavel/jetbot_orgon_ws/src/install/aruco_msgs/share/aruco_msgs/package.sh"
--- build/aruco_msgs/rosidl_cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(aruco_msgs_IDL_FILES "msg/ArucoMarker.idl;msg/ArucoMarkers.idl")
-set(aruco_msgs_INTERFACE_FILES "msg/ArucoMarker.msg;msg/ArucoMarkers.msg")
